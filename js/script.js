d4ac8774f3c3cd4011a0f88760ade3a79a9c24ca
Kirill <94167054+Zavorotnii@users.noreply.github.com>
2022-02-01 11:21:13 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,64 +1,16 @@
+setInterval(bg1, 12000);
 
-function clickDown() {
-    let offerDiv = document.getElementById("hide");
-    let arrowDown = document.getElementById("arrow_down");
-    let arrowUp = document.getElementById("arrow_up");
-    arrowDown.style.display = "none";
-    arrowUp.style.display = "inline-block";
-    offerDiv.style.display = "flex";
+function bg1() {
+    document.getElementById("header").style.backgroundImage = "url(/img/img1.png)"
+    setTimeout(bg2, 4000);
 }
 
-
-function clickUp() {
-    let offerDiv = document.getElementById("hide");
-    let arrowDown = document.getElementById("arrow_down");
-    let arrowUp = document.getElementById("arrow_up");
-    arrowDown.style.display = "inline-block";
-    arrowUp.style.display = "none";
-    offerDiv.style.display = "none";
+function bg2() {
+    document.getElementById("header").style.backgroundImage = "url(/img/img3.png)"
+    setTimeout(bg3, 4000);
 }
 
-// if (screen.width < 767) {
-//     viewport = document.querySelector("meta[name=viewport]");
-//     viewport.setAttribute('content', 'width=device-width');
-// }
-
-// if (screen.width > 767) {
-//     viewport = document.querySelector("meta[name=viewport]");
-//     viewport.setAttribute('content', 'width=1920px');
-// }
-
-// window.addEventListener('resize', function (event) {
-
-//     if (screen.width < 767) {
-//         viewport = document.querySelector("meta[name=viewport]");
-//         viewport.setAttribute('content', 'width=device-width');
-//     }
-
-//     if (screen.width > 767) {
-//         viewport = document.querySelector("meta[name=viewport]");
-//         viewport.setAttribute('content', 'width=1920px');
-//     }
-
-// }, true);
-
-
-//FOR OLD BROWSERS==================================================================================================================
-function testWebP(callback) {
-
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
-        callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+function bg3() {
+    document.getElementById("header").style.backgroundImage = "url(/img/img2.png)"
 }
 
-testWebP(function (support) {
-
-    if (support == true) {
-        document.querySelector('body').classList.add('webp');
-    } else {
-        document.querySelector('body').classList.add('no-webp');
-    }
-});
-//==================================================================================================================================
