9e3149f546c4fb9d528b581738fd886cfc3c635a
Kirill <94167054+Zavorotnii@users.noreply.github.com>
2022-06-09 11:38:30 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+document.addEventListener('keyup', (e) => {
+    if (e.code === "Enter") {
+        console.log('getID')
+        getID()
+    }
+    else {
+        console.log('no');
+
+    }
+})
+
+function getFocus() {
+    document.getElementById("id_film").focus();
+}
+
 
 function getID() {
     console.log('ID - ' + document.getElementById("id_film").value);
@@ -26,4 +41,23 @@ function playVideo(id) {
     const sp = document.createElement('script');
     sp.src = "//yohoho.cc/yo.js";
     document.getElementById("content").append(sp)
+}
+
+function createPlayer(linkChange) {
+    const newVideo = document.createElement('video');
+    newVideo.id = 'videoStream';
+    newVideo.className = 'video-js vjs-default-skin';
+    newVideo.setAttribute("controls", "controls");
+    newVideo.setAttribute("muted", "muted");
+    newVideo.setAttribute("autoplay", "any");
+    newVideo.setAttribute('data-setup', '{ "fluid": false,  "inactivityTimeout": 0}');
+    document.getElementById('video').append(newVideo);
+    const newSource = document.createElement('source');
+    newSource.type = "application/x-mpegURL";
+    newSource.src = link + linkChange;
+    document.getElementById('videoStream').appendChild(newSource)
+    const sp = document.createElement('script');
+    sp.setAttribute('type', 'text/javascript');
+    sp.src = "js/hubufm-player.js";
+    document.getElementById('videoStream').appendChild(sp);
 }
